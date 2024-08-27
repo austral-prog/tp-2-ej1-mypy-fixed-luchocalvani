cf4dcd67247fbd8d182a25dc948a855df765003c
luchocalvani <luchocalvani@gmail.com>
2024-08-27 20:30:05 -0300

    entrega

--- src/leap_year.py
+++ src/leap_year.py
@@ -1,3 +1,11 @@
 # Replace the "ANSWER HERE" for your answer
-def is_leap_year():
-    return "ANSWER HERE" # Remove this line and implement
+def is_leap_year() -> bool:
+    año: int = int(input("ingrese un año: "))
+    if (año % 4 == 0 and año % 100 != 0) or (año % 100 == 0 and año % 400 == 0):
+        print(f"el año {año} es biciesto")
+        return True
+    else:
+        print(f"el año {año} no es biciesto")
+        return False
+
+is_leap_year()
